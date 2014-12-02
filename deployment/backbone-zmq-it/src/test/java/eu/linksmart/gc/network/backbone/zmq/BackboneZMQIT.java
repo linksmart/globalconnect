d47de96a331bfb4a4f17144f4e5f59dab314d7e6
Hassan Rasheed <hassan.rasheed@fit.fraunhofer.de>
2014-12-02 11:22:32 +0100

    backbone-zmq feature & integration test

---
+++ deployment/backbone-zmq-it/src/test/java/eu/linksmart/gc/network/backbone/zmq/BackboneZMQIT.java
@@ -0,0 +1,42 @@
+package eu.linksmart.gc.network.backbone.zmq;
+
+import javax.inject.Inject;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+import eu.linksmart.it.utils.ITConfiguration;
+import eu.linksmart.network.backbone.Backbone;
+
+@RunWith(PaxExam.class)
+public class BackboneZMQIT {
+	
+	@Inject
+    private Backbone backbone;
+
+    @Configuration
+    public Option[] config() {
+        return new Option[] {
+        		ITConfiguration.regressionDefaults(),
+        		features("mvn:eu.linksmart.gc.features/linksmart-gc-features/0.0.1-SNAPSHOT/xml/features","backbone-zmq"),  
+        };
+    }
+    
+    @Test
+    public void testBackbone() {
+    	try {
+    		System.out.println("starting backbone-zmq test");
+            Assert.assertEquals("eu.linksmart.gc.network.backbone.zmq.BackboneZMQImpl",backbone.getClass().getName());
+			System.out.println("backbone-zmq test successfully completed");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("unable to access backboneimpl service");
+		}
+    }
+}
