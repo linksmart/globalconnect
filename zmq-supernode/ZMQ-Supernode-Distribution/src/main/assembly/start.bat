84d7c7e49a53175405ddd6dbdddb3534d4878a40
carlos <karl.catewicz@fit.fraunhofer.de>
2014-12-11 14:25:57 +0100

    - now windows start.bat accepts parameters (NOT TESTED)

--- zmq-supernode/ZMQ-Supernode-Distribution/src/main/assembly/start.bat
+++ zmq-supernode/ZMQ-Supernode-Distribution/src/main/assembly/start.bat
@@ -8,5 +8,5 @@ for /R ./lib %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!"
 echo !CLASSPATH!
 set CURRENTDIR="%cd%"
-java -Dlog4j.debug=true -Dlog4j.configuration=file:%CURRENTDIR%/log4j.properties -client -cp %CLASSPATH% eu.linksmart.global.backbone.zmq.ProxyApplication
+java -Dlog4j.debug=true -Dlog4j.configuration=file:%CURRENTDIR%/log4j.properties -client -cp %CLASSPATH% eu.linksmart.global.backbone.zmq.ProxyApplication %1 %2 %3
 
