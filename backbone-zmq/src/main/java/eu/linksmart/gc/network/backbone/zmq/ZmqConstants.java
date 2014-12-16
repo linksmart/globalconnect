ceaa810265f6fa583a3b3998556b147189a95ac1
Hassan Rasheed <hassan.rasheed@fit.fraunhofer.de>
2014-12-16 16:21:29 +0100

    message_type support added

--- backbone-zmq/src/main/java/eu/linksmart/gc/network/backbone/zmq/ZmqConstants.java
+++ backbone-zmq/src/main/java/eu/linksmart/gc/network/backbone/zmq/ZmqConstants.java
@@ -7,13 +7,10 @@ public class ZmqConstants {
 	public static final String HEARTBEAT_TOPIC = "HEARTBEAT";
 	public static final String BROADCAST_TOPIC = "BROADCAST";
 	
-	public static final byte MESSAGE_TYPE_UNICAST = 0x01;
-	public static final byte MESSAGE_TYPE_PEER_DISCOVERY = 0x02;
+	public static final byte MESSAGE_TYPE_UNICAST_REQUEST = 0x01;
+	public static final byte MESSAGE_TYPE_UNICAST_RESPONSE = 0x02;
 	public static final byte MESSAGE_TYPE_HEART_BEAT = 0x03;
 	public static final byte MESSAGE_TYPE_PEER_DOWN = 0x04;
 	public static final byte MESSAGE_TYPE_SERVICE_DISCOVERY = 0x05;
-	
-	public static final String TYPE_REQUEST = "REQUEST";
-	public static final String TYPE_RESPONSE = "RESPONSE";
 	
 }
