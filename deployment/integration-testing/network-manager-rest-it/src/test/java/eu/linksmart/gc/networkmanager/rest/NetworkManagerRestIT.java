03e288a43fdd0008fce177f73a436250eadf5185
Hassan Rasheed <hassan.rasheed@fit.fraunhofer.de>
2015-03-09 15:28:55 +0100

    extended NM-Rest integration test

--- deployment/integration-testing/network-manager-rest-it/src/test/java/eu/linksmart/gc/networkmanager/rest/NetworkManagerRestIT.java
+++ deployment/integration-testing/network-manager-rest-it/src/test/java/eu/linksmart/gc/networkmanager/rest/NetworkManagerRestIT.java
@@ -2,6 +2,7 @@ package eu.linksmart.gc.networkmanager.rest;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -10,6 +11,7 @@ import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -20,10 +22,8 @@ import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 import static org.junit.Assert.*;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
-
 import eu.linksmart.gc.api.network.ServiceAttribute;
 import eu.linksmart.gc.api.utils.Part;
-
 import eu.linksmart.it.utils.ITConfiguration;
 
 
@@ -40,6 +40,10 @@ public class NetworkManagerRestIT {
 	private static final String KEY_ATTRIBUTES = "Attributes";
 	private static final String KEY_VIRTUAL_ADDRESS = "VirtualAddress";
 	
+	private HttpClient httpClient = null;
+	
+	private String virtualAddress = null;
+	
     @Configuration
     public Option[] config() {
         return new Option[] {
@@ -48,29 +52,71 @@ public class NetworkManagerRestIT {
         };
     }
     
+    @Before
+    public void setUp() {
+    	httpClient = new HttpClient();
+    }
+    
     @Test
-    public void testGetMethod() {
+    public void testNMRest() {
+    	try {
+    		
+    		LOG.info("testing NetworkManager REST");
+
+            // GET
+            testGetByDesc("WeatherService", HttpStatus.SC_OK);
+            testGetByDesc("NoService", HttpStatus.SC_NOT_FOUND);
+
+            // POST
+            testPOST(createPostJSON(), HttpStatus.SC_OK);
+            
+            // PUT
+            testPUT(createPutJSON(), HttpStatus.SC_OK);
+
+            // DELETE
+            testDELETE(virtualAddress, HttpStatus.SC_OK);
+            testDELETE("0.0.0.1", HttpStatus.SC_NOT_FOUND);
+
+            LOG.info("HttpTunnel test successfully completed");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception: " + e.getMessage());
+		}
+	}
+    
+    private void testGetByDesc(String description, int expectedStatus) {
     	
     	try {
-
-    		HttpClient client = new HttpClient();
     		
-    		//
-    		// test service discovery/search by paramerization
-    		//
-    		
-    		LOG.info("testing Get Method: " + base_url);
+    		LOG.info("testing testGetByDesc: " + base_url + " - desc: " + description);
     			  	
         	//
         	// with queryString  ?description=NetworkManager:LinkSmartUser
         	//
-        	NameValuePair[] description_qs = { new NameValuePair("description", "NetworkManager:LinkSmartUser") };
+        	NameValuePair[] description_qs = { new NameValuePair("description", description) };
         	HttpMethod  description_get_request = new GetMethod(base_url);
         	description_get_request.setQueryString(description_qs);
-        	assertEquals(200, client.executeMethod(description_get_request));
-        	System.out.println("description-response-string" + new String(description_get_request.getResponseBody()));
-        	description_get_request.releaseConnection();
         	
+        	int statusCode = this.httpClient.executeMethod(description_get_request);
+        	System.out.println("status-code: " + statusCode);
+        	System.out.println("get-description: " + new String(description_get_request.getResponseBody()));
+        	
+        	description_get_request.releaseConnection();
+        	assertEquals(expectedStatus, statusCode);
+        	
+        	LOG.info("testGetByDesc successfully completed");
+        	
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception: " + e.getMessage());
+		}
+    }
+    
+    private void testGetByOthers(int expectedStatus) {
+    	
+    	try {
+
         	//
         	// with queryString  ?pid=eu.linksmart.network
         	//
@@ -124,19 +170,18 @@ public class NetworkManagerRestIT {
 //        	System.out.println("multi_att_params-response-string" + new String(multi_att_params_qs_get_request.getResponseBody()));
 //        	multi_att_params_qs_get_request.releaseConnection();
         	
-        	LOG.info("testGetMethod successfully completed");
+//        	LOG.info("testGetMethod successfully completed");
         	
 		} catch (Exception e) {
 			e.printStackTrace();
+			fail("Exception: " + e.getMessage());
 		}
     }
     
-    @Test
-    public void testOtherMethods() {
+    //@Test
+    private void testPOST(String jsonString, int expectedStatus) {
     	
     	try {
-    		
-    		HttpClient client = new HttpClient();
     		
     		//
         	// register service
@@ -144,82 +189,130 @@ public class NetworkManagerRestIT {
     		
     		LOG.info("testing POST method: " + base_url);
     		
-    		JSONObject registrationJson = new JSONObject();
-    		
-			registrationJson.put(KEY_ENDPOINT, "http://localhost:9090/cxf/services/Calculator");
-			registrationJson.put(KEY_BACKBONE_NAME, "eu.linksmart.gc.network.backbone.protocol.http.HttpImpl");
-			
-			Part[] attributes = { new Part(ServiceAttribute.DESCRIPTION.name(), "Calculator"), 
-					new Part(ServiceAttribute.SID.name(), "eu.linksmart.gc.testing.calculator") };
-			
-			JSONObject attributesJson = new JSONObject();
-			for(Part p : attributes) {
-				attributesJson.put(p.getKey(), p.getValue());
-			}
-			registrationJson.put(KEY_ATTRIBUTES, attributesJson);
-			
 			PostMethod post_request = new PostMethod(base_url);
 			
-			StringRequestEntity requestEntity = new StringRequestEntity(registrationJson.toString(), "application/json", "UTF-8");
+			StringRequestEntity requestEntity = new StringRequestEntity(jsonString, "application/json", "UTF-8");
 			post_request.setRequestEntity(requestEntity);
 			
-    		assertEquals(200, client.executeMethod(post_request));
+			int statusCode = this.httpClient.executeMethod(post_request);
+			System.out.println("post-status-code: " + statusCode);
+			
     		String registrationJsonString = new String(post_request.getResponseBody());
-        	System.out.println("post-response-string" + registrationJsonString);
+        	System.out.println("post-response: " + registrationJsonString);
+        	
         	post_request.releaseConnection();
+        	assertEquals(expectedStatus, statusCode);
         	
         	JSONObject jsonObject = new JSONObject(registrationJsonString.toString());
-			String virtualAddress = jsonObject.getString(KEY_VIRTUAL_ADDRESS);
+			this.virtualAddress = jsonObject.getString(KEY_VIRTUAL_ADDRESS);
 	
+        	LOG.info("testPOST successfully completed");
+        	
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail("Exception: " + e.getMessage());
+		}
+    }
+    
+    private void testPUT(String jsonString, int expectedStatus) {
+    	
+    	try {
+    		
         	//
         	// updating service
         	//
 			
 			LOG.info("testing PUT method: " + base_url);
         	
-        	JSONObject updateJson = new JSONObject();
-        	
-        	updateJson.put(KEY_ENDPOINT, "http://localhost:9090/cxf/services/Calculator2");
-        	updateJson.put(KEY_BACKBONE_NAME, "eu.linksmart.gc.network.backbone.protocol.http.HttpImpl2");
-        	updateJson.put(KEY_VIRTUAL_ADDRESS, virtualAddress);
-        	
-			Part[] update_attributes = { new Part(ServiceAttribute.DESCRIPTION.name(), "Calculator2"), 
-					new Part(ServiceAttribute.SID.name(), "eu.linksmart.gc.testing.calculator2") };
-
-			JSONObject update_attributesJson = new JSONObject();
-			for(Part p : update_attributes) {
-				update_attributesJson.put(p.getKey(), p.getValue());
-			}
-			updateJson.put(KEY_ATTRIBUTES, update_attributesJson);
-			
 			PutMethod put_request = new PutMethod(base_url);
 			
-			StringRequestEntity put_requestEntity = new StringRequestEntity(updateJson.toString(), "application/json", "UTF-8");
+			StringRequestEntity put_requestEntity = new StringRequestEntity(jsonString, "application/json", "UTF-8");
 			put_request.setRequestEntity(put_requestEntity);
 			
-    		assertEquals(200, client.executeMethod(put_request));
+			int statusCode = this.httpClient.executeMethod(put_request);
+			System.out.println("put-status-code: " + statusCode);
+			
     		String updateJsonString = new String(put_request.getResponseBody());
-        	System.out.println("put-response-string" + updateJsonString);
+        	System.out.println("put-response: " + updateJsonString);
+        	
         	put_request.releaseConnection();
+        	assertEquals(expectedStatus, statusCode);
         	
         	JSONObject updateJsonObject = new JSONObject(updateJsonString.toString());
-			String updated_virtualAddress = updateJsonObject.getString(KEY_VIRTUAL_ADDRESS);
+			this.virtualAddress = updateJsonObject.getString(KEY_VIRTUAL_ADDRESS);
 			
-			//
-        	// removing service
-        	//
-			
-			LOG.info("testing DELETE method: " + base_url);
-			
-        	DeleteMethod delete_request = new DeleteMethod(base_url + "/" + updated_virtualAddress);
-			assertEquals(200, client.executeMethod(delete_request));
-			System.out.println("delete-response-string" + new String(delete_request.getResponseBody()));
-        	delete_request.releaseConnection();
-        	
-        	LOG.info("testOtherMethods successfully completed");
+        	LOG.info("testPUT successfully completed");
         	
 		} catch (Exception e) {
 			e.printStackTrace();
+			fail("Exception: " + e.getMessage());
 		}
+    }
+
+    private void testDELETE(String virtualAddress, int expectedStatus) {
+    	
+    	try {
+    		
+    		//
+        	// removing service
+        	//
+    		
+    		LOG.info("testing DELETE method: " + base_url + " for VAD: " + virtualAddress);
+    		
+        	DeleteMethod delete_request = new DeleteMethod(base_url + "/" + virtualAddress);
+        	
+        	int statusCode = this.httpClient.executeMethod(delete_request);
+    		System.out.println("delete-status-code: " + statusCode);
+    		
+    		System.out.println("delete-response: " + new String(delete_request.getResponseBody()));
+        	
+    		delete_request.releaseConnection();
+    		assertEquals(expectedStatus, statusCode);
+    		
+        	LOG.info("testDELETE successfully completed");
+        	
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		fail("Exception: " + e.getMessage());
+    	}
+    }
+    
+    private String createPostJSON() throws Exception {
+    	
+    	JSONObject registrationJson = new JSONObject();
+		
+		registrationJson.put(KEY_ENDPOINT, "http://localhost:8882/WeatherService");
+		registrationJson.put(KEY_BACKBONE_NAME, "eu.linksmart.gc.network.backbone.protocol.http.HttpImpl");
+		
+		Part[] attributes = { new Part(ServiceAttribute.DESCRIPTION.name(), "WeatherService"), 
+				new Part(ServiceAttribute.SID.name(), "eu.linksmart.gc.example.weatherservice") };
+		
+		JSONObject attributesJson = new JSONObject();
+		for(Part p : attributes) {
+			attributesJson.put(p.getKey(), p.getValue());
+		}
+		registrationJson.put(KEY_ATTRIBUTES, attributesJson);
+		
+    	return registrationJson.toString();
+    }
+    
+    private String createPutJSON() throws Exception {
+    	
+    	JSONObject updateJson = new JSONObject();
+    	
+    	updateJson.put(KEY_ENDPOINT, "http://localhost:8882/WeatherService2");
+    	updateJson.put(KEY_BACKBONE_NAME, "eu.linksmart.gc.network.backbone.protocol.http.HttpImpl");
+    	updateJson.put(KEY_VIRTUAL_ADDRESS, this.virtualAddress);
+    	
+		Part[] update_attributes = { new Part(ServiceAttribute.DESCRIPTION.name(), "WeatherService2"), 
+				new Part(ServiceAttribute.SID.name(), "eu.linksmart.gc.example.weatherservice2") };
+
+		JSONObject update_attributesJson = new JSONObject();
+		for(Part p : update_attributes) {
+			update_attributesJson.put(p.getKey(), p.getValue());
+		}
+		updateJson.put(KEY_ATTRIBUTES, update_attributesJson);
+    	
+		return updateJson.toString();
     }
 }
