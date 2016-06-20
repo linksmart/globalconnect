50e60bc06de7bd645d947ce4bb2e1c5c6b77a67e
Raphael Ahrens <raphael.ahrens@fit.fraunhofer.de>
2016-06-20 14:09:10 +0200

    rewrote the ZMQ-SN start script to use POSIX sh
    Some system don't have bash by default, so it makes more sense to use
    /bin/sh instead of /bin/bash.

--- zmq-supernode/ZMQ-Supernode-Distribution/src/main/assembly/start.sh
+++ zmq-supernode/ZMQ-Supernode-Distribution/src/main/assembly/start.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 CP=""
 
 for jar in lib/*.jar; do
-CP+=$jar:
+    CP="$CP$jar:"
 done
 
-java -client -Dlog4j.debug=true -Dlog4j.configuration=file:$PWD/log4j.properties -cp $CP eu.linksmart.gc.supernode.ProxyApplication "$@";
+java -client -Dlog4j.debug=true -Dlog4j.configuration=file:"$PWD"/log4j.properties -cp "$CP" eu.linksmart.gc.supernode.ProxyApplication "$@";
